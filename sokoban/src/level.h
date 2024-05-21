47ed5e3046a12a1f92a73b0f62a55a9b781483f8
Atai <ataydjirgalbaev@gmail.com>
2024-05-21 18:21:44 +0600

    add level class layout

--- sokoban/src/level.h
+++ sokoban/src/level.h
@@ -5,16 +5,14 @@
 
 class level {
 public:
-    Level();
-    ~Level();
+    level();
+    ~level();
 
     void load_level(const level& lvl);
     void unload_level();
-    bool is_cell_inside_level(int row, int column) const;
-    char& get_level_cell(size_t row, size_t column) const;
+    [[nodiscard]] bool is_cell_inside_level(int row, int column) const;
+    [[nodiscard]] char& get_level_cell(size_t row, size_t column) const;
     void set_level_cell(size_t row, size_t column, char cell);
-    void display() const;
-    void update();
 
 private:
     size_t rows, columns;
